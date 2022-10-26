7acc47a58c8befa926364def95d776cbcac27f19
Sharon <sreeammu6d@gmail.com>
2022-10-26 15:35:54 +0530

    creating test

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,15 +1,56 @@
 import pytest
+import json
+import logging
+import os
+import joblib
+import pytest
+from prediction_service.prediction import form_response, api_response
+import prediction_service
+
+TARGET_range = {
+    "min": 3.0,
+    "max": 8.0
+}
+input_data = {
+    "correct_range":
+    {
+
+        "fixed_acidity": 5,
+        "volatile_acidity": 0.5,
+        "citric_acid": 0.99,
+        "residual_sugar": 14,
+        "chlorides": 0.5,
+        "free_sulfur_dioxide": 5,
+        "total_sulfur_dioxide": 75,
+        "density": 1,
+        "pH": 3,
+        "sulphates": 1,
+        "alcohol": 9
+
+    },
+    "incorrect_range":
+    {
+
+        "fixed_acidity": 5,
+        "volatile_acidity": 0.5,
+        "citric_acid": 0.99,
+        "residual_sugar": 14,
+        "chlorides": 0.5,
+        "free_sulfur_dioxide": 999,
+        "total_sulfur_dioxide": 75,
+        "density": 1,
+        "pH": 3,
+        "sulphates": 1,
+        "alcohol": 9
+    }
+}
 
 
-class NotInRange(Exception):
-    def __init__(self, message = "value not in range"):
-        #self.input_= input_
-        self.message = message
-        super().__init__(self.message)
+def test_form_response_correct_range(data=input_data["correct_range"]):
+    res = form_response(data)
+    assert TARGET_range["min"] <= res <= TARGET_range["max"]
 
-def test_generic():
-    a = 5
-    with pytest.raises(NotInRange):
-        if a not in range(10, 20):
-            raise NotInRange
-        
+
+def test_api_response_correct_range(data=input_data["correct_range"]):
+    res = api_response(data)
+    assert TARGET_range["min"] <= res["response"] <= TARGET_range["max"]
